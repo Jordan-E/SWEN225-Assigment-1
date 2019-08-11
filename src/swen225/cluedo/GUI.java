273a86849ab3d10cc221f6b11ce392d0798f0cf4
Jordan-E <38929352+Jordan-E@users.noreply.github.com>
2019-08-12 00:24:36 +1200

    Add files via upload

---
+++ src/swen225/cluedo/GUI.java
@@ -0,0 +1,40 @@
+import java.awt.BorderLayout;
+
+import javax.swing.*;
+
+/**
+ * GUI class to display the board. Extends JFrame so so instance of JFrame isnt needed
+ * @author jordi
+ *
+ */
+public class GUI extends JFrame { 
+	
+	JFrame frame;
+	
+	public GUI(){
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(1024,576);
+		setLocationRelativeTo(null);	//displays in the middle of the display
+
+		
+		JMenuBar mb = new JMenuBar();
+        JMenu m1 = new JMenu("FILE");
+        JMenu m2 = new JMenu("Game");
+        mb.add(m1);
+        mb.add(m2);
+        JMenuItem m11 = new JMenuItem("Open");
+        JMenuItem m22 = new JMenuItem("Save as");
+        m1.add(m11);
+        m1.add(m22);
+        
+        
+        getContentPane().add(BorderLayout.NORTH, mb);
+        setVisible(true); //makes the window visible
+	}
+	
+	
+	public static void main(String args[]){
+		new GUI();
+	}
+}
